a5cf5c7f72fadc4c16f8ff363bfb97d818919376
david12-ops <77929483+david12-ops@users.noreply.github.com>
2025-04-11 17:40:08 +0200

    controller update

--- src/main/java/com/example/constroller/UserController.java
+++ src/main/java/com/example/constroller/UserController.java
@@ -3,14 +3,15 @@ package com.example.constroller;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
-
-import org.mindrot.jbcrypt.BCrypt;
+import java.util.List;
 
 import com.example.model.User;
 import com.example.model.UserModel;
 import com.example.model.UserToken;
 import com.example.utils.ImageConvertor;
 import com.example.utils.SessionHolder;
+import com.example.utils.enums.AddTypeOperation;
+import com.example.utils.enums.GetUserTypeOperation;
 import com.example.utils.interfaces.AuthManagement;
 import com.example.utils.interfaces.UserManagement;
 import com.example.utils.services.SessionService;
@@ -21,17 +22,24 @@ public class UserController implements AuthManagement, UserManagement {
     private UserModel model;
     private SessionService service;
 
-    // In this user cannot update email adress only password of account
-    // TODO - when messages will be done - can add functionality to update email
-    // adress of account
+    // TODOD - support switching - yes - implement group Id - need gui
+    // TODO - app bar - (logo - search bar - Avatar (drop menu) - log out)
 
     public UserController(UserModel model) {
         this.model = model;
         this.service = SessionService.getInstance();
     }
 
-    private User getUser(String emailAccount, String password, UserToken userToken) {
-        return model.getUserByCredentials(emailAccount, password, userToken);
+    private User getUser(String emailAccount, String password, GetUserTypeOperation getUserTypeOperation) {
+        if (getUserTypeOperation == GetUserTypeOperation.CURRENT) {
+            UserToken userToken = getLoggedUser();
+            return model.getUserByCredentials(null, null, userToken);
+        }
+
+        if (getUserTypeOperation == GetUserTypeOperation.BYCREDENTIALS) {
+            return model.getUserByCredentials(emailAccount, password, null);
+        }
+        return null;
     }
 
     public HashMap<String, String> getInputErrors() {
@@ -41,19 +49,17 @@ public class UserController implements AuthManagement, UserManagement {
     // Auth
     @Override
     public boolean register(String emailAccount, String password) {
-        model.addUser(emailAccount, password);
-        return getUser(emailAccount, password, null) != null ? true : false;
+        model.addUser(emailAccount, password, null, AddTypeOperation.NEWACCOUNT);
+        return getUser(emailAccount, password, GetUserTypeOperation.BYCREDENTIALS) != null ? true : false;
     }
 
     @Override
     public void login(String emailAccount, String password) {
-        User user = getUser(emailAccount, password, null);
+        User user = getUser(emailAccount, password, GetUserTypeOperation.BYCREDENTIALS);
 
-        if (user != null && BCrypt.checkpw(password, user.getPassword())) {
-            if (!this.service.isUserLoggedIn(user.getId())) {
-                String sessionId = this.service.createSessionId(user);
-                SessionHolder.setSessionId(sessionId);
-            }
+        if (user != null && !this.service.isUserLoggedIn(user.getUserId())) {
+            String sessionId = this.service.createSessionId(user);
+            SessionHolder.setSessionId(sessionId);
         }
     }
 
@@ -61,10 +67,10 @@ public class UserController implements AuthManagement, UserManagement {
     public boolean updateNotLoggedAccount(String emailAccount, String password, String newPassword,
             String confirmationPassword) {
 
-        User foundUser = getUser(emailAccount, password, null);
+        User foundUser = getUser(emailAccount, password, GetUserTypeOperation.BYCREDENTIALS);
 
         model.updateUser(foundUser, newPassword, confirmationPassword);
-        return getUser(emailAccount, confirmationPassword, null) != null ? true : false;
+        return getUser(emailAccount, confirmationPassword, GetUserTypeOperation.BYCREDENTIALS) != null ? true : false;
     }
 
     @Override
@@ -90,29 +96,9 @@ public class UserController implements AuthManagement, UserManagement {
         }
     }
 
-    // User actions
     @Override
-    public void addAccount(String emailAccount, String password) {
-        model.addUser(emailAccount, password);
-    }
-
-    @Override
-    public void removeAccount(User user) {
-        model.removeUser(getLoggedUser(), user);
-    }
-
-    @Override
-    public boolean updateLoggedInAccount(String newPassword, String confirmationPassword) {
-
-        UserToken userToken = getLoggedUser();
-
-        model.updateUser(userToken, newPassword, confirmationPassword);
-        return getUser(userToken.getEmail(), confirmationPassword, null) != null ? true : false;
-    }
-
-    @Override
-    public Image getImageProfile(UserToken userToken) {
-        User user = getUser(null, null, userToken);
+    public Image getImageProfile() {
+        User user = getUser(null, null, GetUserTypeOperation.CURRENT);
 
         if (user != null && user.getProfileImage() != null) {
             return ImageConvertor.Base64ToImage(user.getProfileImage());
@@ -120,14 +106,60 @@ public class UserController implements AuthManagement, UserManagement {
         return null;
     }
 
+    // User actions
     @Override
-    public void AddImageProfile(File file, UserToken userToken) {
+    public void removeAccount(User user) {
+        UserToken userToken = getLoggedUser();
+        model.removeUser(userToken, user);
+    }
+
+    @Override
+    public boolean updateLoggedInAccount(String newPassword, String confirmationPassword) {
+        UserToken userToken = getLoggedUser();
+        System.out.println(userToken.getMailAccount());
+
+        model.updateUser(userToken, newPassword, confirmationPassword);
+        return getUser(userToken.getMailAccount(), confirmationPassword, GetUserTypeOperation.BYCREDENTIALS) != null
+                ? true
+                : false;
+    }
+
+    @Override
+    public void AddImageProfile(File file) {
         try {
+            UserToken userToken = getLoggedUser();
             String base64 = ImageConvertor.imageToBase64(file);
             model.updateUser(userToken, base64);
         } catch (IOException e) {
             System.err.println("Error converting image: " + e.getMessage());
             // Optionally show Alert dialog here
         }
+    }
+
+    @Override
+    public void addAnotherAccount(String emailAccount, String password) {
+        UserToken userToken = getLoggedUser();
+        model.addUser(emailAccount, password, userToken, AddTypeOperation.ANOTHERACCOUNT);
+    }
+
+    @Override
+    public void switchAccount(User switchtoUser) {
+        UserToken userToken = getLoggedUser();
+
+        if (userToken != null && switchtoUser != null
+                && !userToken.getMailAccount().equals(switchtoUser.getMailAccount())) {
+            SessionHolder.setSessionId(null);
+            String sessionId = this.service.createSessionId(switchtoUser);
+            SessionHolder.setSessionId(sessionId);
+
+            // after this view - screenController will reload and active mainScreen with new
+            // session
+        }
+    }
+
+    @Override
+    public List<User> getAllUserAccounts() {
+        UserToken userToken = getLoggedUser();
+        return model.getAllUserAccounts(userToken);
     }
 }
--- src/main/java/com/example/constroller/messageController.java
+++ src/main/java/com/example/constroller/messageController.java
@@ -16,9 +16,13 @@ public class MessageController {
         this.model = model;
     }
 
-    public void addMesssage(String subject, String message, String senderId, String recevierId) {
-        Message newMessage = new Message(senderId, recevierId, subject, message, LocalDateTime.now());
+    public void sendMessage(String subject, String message, String senderId, String recevierId) {
+        Message newMessage = new Message(null, senderId, recevierId, subject, message, LocalDateTime.now());
         model.addMesssage(newMessage);
+    }
+
+    public void responseToMessage() {
+        throw new UnsupportedOperationException("Unimplemented method 'responseToMessage'");
     }
 
     public void removeMessage(Message message) {
@@ -29,11 +33,11 @@ public class MessageController {
         List<Message> messages = new ArrayList<>();
 
         if (type == MessageType.SENDED && userToken != null) {
-            messages = model.getAllSendedMessagesByUser(userToken.getId());
+            messages = model.getAllSendedMessagesByUser(userToken.getUserId());
         }
 
         if (type == MessageType.RECEVIED && userToken != null) {
-            messages = model.getAllReceviedMessagesByUser(userToken.getId());
+            messages = model.getAllReceviedMessagesByUser(userToken.getUserId());
         }
 
         return messages;
