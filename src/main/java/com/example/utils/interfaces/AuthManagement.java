c6597e971cef90856a38d755c103b8ea240e5a1b
david12-ops <77929483+david12-ops@users.noreply.github.com>
2025-04-11 17:35:22 +0200

    added features

--- src/main/java/com/example/utils/interfaces/AuthManagement.java
+++ src/main/java/com/example/utils/interfaces/AuthManagement.java
@@ -1,6 +1,11 @@
 package com.example.utils.interfaces;
 
+import java.util.List;
+
+import com.example.model.User;
 import com.example.model.UserToken;
+
+import javafx.scene.image.Image;
 
 public interface AuthManagement {
 
@@ -12,7 +17,10 @@ public interface AuthManagement {
 
     UserToken getLoggedUser();
 
+    List<User> getAllUserAccounts();
+
     boolean updateNotLoggedAccount(String emailAccount, String password, String newPassword,
             String confirmationPassword);
 
+    Image getImageProfile();
 }
--- src/main/java/com/example/utils/interfaces/UserManagement.java
+++ src/main/java/com/example/utils/interfaces/UserManagement.java
@@ -1,21 +1,19 @@
 package com.example.utils.interfaces;
 
 import com.example.model.User;
-import com.example.model.UserToken;
 
-import javafx.scene.image.Image;
 import java.io.File;
 import java.io.IOException;
 
 public interface UserManagement {
 
-    void addAccount(String emailAccount, String password);
+    void addAnotherAccount(String emailAccount, String password);
+
+    void switchAccount(User switchtoUser);
 
     void removeAccount(User user);
 
     boolean updateLoggedInAccount(String newPassword, String confirmationPassword);
 
-    Image getImageProfile(UserToken userToken);
-
-    void AddImageProfile(File file, UserToken userToken) throws IOException;
+    void AddImageProfile(File file) throws IOException;
 }
